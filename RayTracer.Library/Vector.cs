44ef15e7894853abfb899df929dcde8ba916a5cb
Graham Smith <graham.david.smith@gmail.com>
2019-06-05 21:46:59 +0100

    Magnitude

--- RayTracer.Library/Vector.cs
+++ RayTracer.Library/Vector.cs
@@ -6,6 +6,11 @@ namespace RayTracer.Library
 {
     public class Vector : Tuple
     {
-        public Vector(double x, double y, double z) : base(x, y, z, 0){}
+        public Vector(double x, double y, double z) : base(x, y, z, 0) { }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
     }
 }
--- RayTracer.UnitTests/VectorTests.cs
+++ RayTracer.UnitTests/VectorTests.cs
@@ -16,5 +16,29 @@ namespace RayTracer.UnitTests
             Assert.Equal(t, v);
             Assert.Equal(v, t);
         }
+
+        [Theory]
+        [InlineData(1,1,0,0)]
+        [InlineData(1, 0, 1, 0)]
+        [InlineData(1, 0, 0, 1)]
+        public void Can_Compute_Magnitude(double expected, double x, double y, double z)
+        {
+            //Given
+            var v = new Vector(x, y, z);
+
+            //Then
+            Assert.Equal(expected, v.Magnitude());
+        }
+
+        [Fact]
+        public void Can_Compute_Magnitude_Root()
+        {
+            //Given
+            var v = new Vector(1,2,3);
+            var expected = System.Math.Sqrt(14);
+
+            //Then
+            Assert.Equal(expected, v.Magnitude());
+        }
     }
 }
